2f5f0ff74e30b27232a8d36c654c8fe8c61c8c61
ScreamKing13 <scremking@gmail.com>
2018-10-05 20:35:04 +0300

    Добавлен срез(срезы)

--- Lab_3.py
+++ Lab_3.py
@@ -2,13 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
+# Наилучшие значения
+#a = -0.4
+#b = -0.25
+
+
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-a = 1
-b = 1
-x = np.arange(-50, 50)
-y = np.arange(-50, 50)
+#ax.set_zlim()
+a = -0.4
+b = -0.25
+x = np.arange(-20, 20)
+y = np.arange(-15, 15)
+x2 = 0
+y2 = y
+x2, y2 = np.meshgrid(x2, y2)
 x, y = np.meshgrid(x, y)
 z = (x-a)**2/a - (y-b)**2/b
 surf = ax.plot_surface(x, y, z, linewidth=0, antialiased=True)
+for i in range(-750, 1000, 25):
+    ax.plot3D(xs=x2, ys=y2, zs=i, antialiased=True, color="r")
 plt.show()
